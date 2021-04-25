4d21554b808f60259856d53458bf7807b4e35d20
Lukas Belay <lukasbelay71@gmail.com>
2021-04-25 20:27:24 +0200

    FirstTouch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import FC from "./Components/FunctionalComp"
+import Click from "./Components/Click"
+import ParentComp from "./Components/ParentComp"
+import Counter from "./Components/Counter"
+import {ClassComp, ClassComp1} from './Components/ClassComp';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div>
+            <h1>Hello world!</h1>
+            <h1>This is my first ReactApp!</h1>
+            <FC />
+            <ClassComp />
+            <ClassComp1 />
+            <Click />
+            <Counter />
+            <ParentComp />
+        </div>
 }
 
 export default App;
---
+++ src/Components/ClassComp.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export class ClassComp extends React.Component{
+    render()
+    {
+        return <p>This is a first ClassComponent</p>
+    }
+}
+
+export class ClassComp1 extends React.Component{
+    render()
+    {
+        return <p>Hey! I am another Class</p>
+    }
+}
+
+export default ClassComp;
